7fa84e468c4365700c29ebd57f979085f8206461
Mark <mark.revill@servian.com>
2021-09-11 17:07:19 +1000

    gh workflow testing

--- infra/main.tf
+++ infra/main.tf
@@ -10,11 +10,6 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
 }
 
 resource "azurerm_resource_group" "example" {
@@ -27,22 +22,22 @@ variable environment {
   description = "description"
 }
 
-variable subscription_id {
-  type        = string
-  description = "description"
-}
+# variable subscription_id {
+#   type        = string
+#   description = "description"
+# }
 
-variable client_id {
-  type        = string
-  description = "description"
-}
+# variable client_id {
+#   type        = string
+#   description = "description"
+# }
 
-variable client_secret {
-  type        = string
-  description = "description"
-}
+# variable client_secret {
+#   type        = string
+#   description = "description"
+# }
 
-variable tenant_id {
-  type        = string
-  description = "description"
-}
+# variable tenant_id {
+#   type        = string
+#   description = "description"
+# }
